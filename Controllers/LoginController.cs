6736b5baa5382004eeb629ef652d77761beccdb2
Kaio Maciel <43120150+kaiogotyacode@users.noreply.github.com>
2023-08-11 09:18:50 -0300

    Created Login/Signup Forms

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LoginAuth.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}
