7c891df75fbf7c279c46654fcf518fdc8bc79957
Kaio Maciel <43120150+kaiogotyacode@users.noreply.github.com>
2023-08-16 09:08:18 -0300

    Added Essentials for Reviewing

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LoginAuth.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+        
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+    }
+}
